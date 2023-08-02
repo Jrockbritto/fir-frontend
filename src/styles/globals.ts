bea2abb82970336a308d8eac1288574b165ca628
Joaorock <jrockbritto@gmail.com>
2023-08-02 15:17:10 -0300

    fix: margin and padding in all contexts

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,6 +1,15 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      
+  }
+
   .main {
     display: flex;
     flex-direction: column;
